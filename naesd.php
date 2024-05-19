601d47ae72886ccd38c7a41766eec019e31a3bca
shamsulkgh <100175768+shamsulkgh@users.noreply.github.com>
2024-05-19 10:55:38 +0800

    Create naesd.php

---
+++ naesd.php
@@ -0,0 +1,34 @@
+<?php
+
+if(!empty($_POST['iv']))
+{
+	if(strlen($_POST['iv'])==16){
+	
+    	$string=$_POST['string'];
+    	$password =$_POST['pass'];
+    	$method = "AES-256-CBC";
+    	$iv = $_POST['iv'];
+
+    	$decrypted_string=openssl_decrypt($string, $method, $password, 0, $iv);
+    	response(200,"Valid Request",$decrypted_string);	
+    }
+    else{
+    	response(400,"Invalid Request. The seed is not 16 bytes long",NULL);
+    }
+}
+else
+{
+	response(400,"Invalid Request. The seed is empty. It should be 16 bytes long.",NULL);
+}
+
+
+function response($status,$status_message,$data)
+{
+	header("HTTP/1.1 ".$status);
+
+	//echo $status_message
+
+	echo $data;
+}
+
+?>
